c35b916a85b1648b84ab5ebe72c64591dd5ac122
Andre Restivo <andre.restivo@gmail.com>
2017-03-28 23:52:48 +0100

    Added some comments to the ShipModel class

--- core/src/com/aor/arena/model/entities/ShipModel.java
+++ core/src/com/aor/arena/model/entities/ShipModel.java
@@ -4,16 +4,36 @@ package com.aor.arena.model.entities;
  * A model representing a the user space ship.
  */
 public class ShipModel extends EntityModel {
+    /**
+     * Is this ship accelerating in this update delta
+     */
     private boolean accelerating = true;
 
+    /**
+     * Creates a new ship model in a certain position and having a certain rotation.
+     *
+     * @param x the x-coordinate in meters
+     * @param y the y-coordinate in meters
+     * @param rotation the rotation in radians
+     */
     public ShipModel(float x, float y, int rotation) {
         super(x, y, rotation);
     }
 
+    /**
+     * Set the accelerating flag for this ship
+     *
+     * @param accelerating the accelerating tag
+     */
     public void setAccelerating(boolean accelerating) {
         this.accelerating = accelerating;
     }
 
+    /**
+     * Is the ship accelerating in this update
+     *
+     * @return the accelerating flag
+     */
     public boolean isAccelerating() {
         return accelerating;
     }
